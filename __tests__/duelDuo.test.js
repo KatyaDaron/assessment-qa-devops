06c6301bd3c4d1a1e5d459e35fd49e6a0f562749
Katya Daron <katya_daron@hotmail.com>
2023-05-13 23:49:50 -0400

    Part 4: Write Automated Tests

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,26 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+
+  test("clicking the Draw button displays the div with id = “choices”", async () => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.css("#draw")).click();
+    const choicesDiv = await driver.wait(until.elementLocated(By.css("#choices")), 1000);
+    expect(await choicesDiv.isDisplayed()).toBeTruthy();
+  });
+
+  test("clicking an “Add to Duo” button displays the div with id = “player-duo”", async () => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.css("#draw")).click();
+    await driver.findElement(By.css(".bot-btn")).click();
+    const playerDuoDiv = await driver.wait(until.elementLocated(By.css("#player-duo")), 1000);
+    expect(await playerDuoDiv.isDisplayed()).toBeTruthy();
+  });
+
+  test("clicking the See All Bots button displays the div with id = “all-bots”", async () => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.css("#see-all")).click();
+    const botsDiv = await driver.wait(until.elementLocated(By.css("#all-bots")), 1000);
+    expect(await botsDiv.isDisplayed()).toBeTruthy();
+  });
 });
